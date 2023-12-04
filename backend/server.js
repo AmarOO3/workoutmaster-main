d6612fb98900a60705a9bfa52c27d21d4f36cc91
AmarOO3 <141107370+AmarOO3@users.noreply.github.com>
2023-12-04 21:57:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,35 @@
-
-// const mongodb = require('mongodb') 
-const express = require('express')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
-const mongoPass= "ZNsFm6WKpJPmzHJ0"
-const mongoURI = `mongodb+srv://amarjeetabdar:${mongoPass}@gymdata.rrq4us9.mongodb.net/?retryWrites=true&w=majority`
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import cors middleware
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
+const mongoPass = "ZNsFm6WKpJPmzHJ0";
+const mongoURI = `mongodb+srv://amarjeetabdar:${mongoPass}@gymdata.rrq4us9.mongodb.net/?retryWrites=true&w=majority`;
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
-
-app.use((req, res, next)=>{
-    console.log(req.path, req.method)
-    next()
-})
+app.use(express.json());
+app.use(cors()); // Use cors middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
 
 // routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 // connect to db
 mongoose.connect(mongoURI)
-    .then(()=>{
+    .then(() => {
         // listen for requests
-        app.listen(4000, ()=>{
-            console.log("Connected to db. Listening on port " + 4000    )
-        })
+        app.listen(4000, () => {
+            console.log("Connected to db. Listening on port " + 4000);
+        });
     })
-    .catch((error=>{
-        console.log(error)
-        process.exit(1)
-    }))    
-   
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
